d2de8cc0496625c2fc5b7c2f28554506266d70e9
sunup2015 <30277413+sunup2015@users.noreply.github.com>
2019-10-12 13:44:16 +0800

    Add files via upload

---
+++ resources/themes/bootstrap/default_footer.php
@@ -0,0 +1,8 @@
+<?php
+ini_set('date.timezone','Asia/Shanghai');
+?>
+<footer class="container">
+  <div class="footer">
+ © 2015-<?php echo date("Y")?> <a href="/" target="_blank">逗比网络</a> . All rights reserved.
+   </div>
+</footer>
---
+++ resources/themes/bootstrap/default_header.php
@@ -0,0 +1,4 @@
+<!--本文件可以放置 统计代码等。-->
+<?php
+ini_set('date.timezone','Asia/Shanghai');
+?>
